1bdab41aeedbc2948e8cf23d8a5eb0c95293d798
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-01-12 13:41:11 +0100

    PlayerScoreRace from fouc

--- types/ScoreManager.h
+++ types/ScoreManager.h
@@ -1,10 +1,37 @@
 class PlayerScoreRace {
 public:
-	uint8_t _0[0x4];
 	uint32_t nPlayerId; // +4
 	int32_t nPosition; // +8
-	uint8_t _C[0x38];
+	uint8_t _C[0x2C];
+	bool bHasFinished; // +38
+	bool bIsDNF; // +39
+	uint8_t _3A[0x2];
+	float fProgress; // +3C
+	uint8_t _40[0x4];
 	uint32_t nCurrentLap; // +44
+	uint32_t nLapTimes[10]; // +48
+	uint8_t _70[0x4];
+	uint32_t nBestLapTime; // +74
+	uint32_t nFinishTime; // +78
+
+	virtual void _vf0() = 0;
+	virtual void _vf1() = 0;
+	virtual void _vf2() = 0;
+	virtual void _vf3() = 0;
+	virtual void _vf4() = 0;
+	virtual bool HasFinished() = 0;
+	virtual bool IsDNF() = 0;
+	virtual uint32_t GetFinishTime() = 0;
+	virtual void _vf8() = 0;
+	virtual void _vf9() = 0;
+	virtual void _vf10() = 0;
+	virtual void _vf11() = 0;
+	virtual void _vf12() = 0;
+	virtual void _vf13() = 0;
+	virtual void _vf14() = 0;
+	virtual void _vf15() = 0;
+	virtual void _vf16() = 0;
+	virtual void _vf17() = 0;
 };
 
 class PlayerScoreDerby {
